b31cc23811a53338a4caede7198b057c40896f17
Rem0o <rem54321@hotmail.com>
2018-10-20 17:26:23 -0400

    Initial commit

---
+++ DynamicMapper.Test/DynamicMapperContainerTest.cs
@@ -0,0 +1,124 @@
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using Xunit;
+
+namespace DynamicMapper.Test
+{
+    public class POCO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int? Count { get; set; }
+        public long Long { get; set; }
+        public bool Question { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is POCO casted)
+                return this.GetType()
+                    .GetProperties()
+                    .All(prop => prop.GetValue(this)?.Equals(prop.GetValue(obj)) ?? prop.GetValue(obj) == null);
+
+            return false;
+        }
+    }
+
+    public class DynamicMapperContainerTest
+    {
+        private Func<bool> ReadCreator(int i)
+        {
+            int c = i;
+            return () => c-- > 0;
+        }
+
+        private readonly Dictionary<Type, object> dic = new Dictionary<Type, object>()
+            {
+                { typeof(Guid), Guid.NewGuid() },
+                { typeof(string), "Hola" },
+                { typeof(DateTime), DateTime.Now },
+                { typeof(int?), null },
+                { typeof(long), 123_456_789_111 },
+                { typeof(bool), false },
+            };
+
+        private Func<object> MapperCreator(Type t) => () => dic.GetValueOrDefault(t);
+
+        private IDataReader GetDataReaderMock()
+        {
+            var mock = new Mock<IDataReader>();
+
+            mock.Setup(dr => dr.Read()).Returns(ReadCreator(1000));
+            foreach (var p in typeof(POCO).GetProperties())
+                mock.Setup(dr => dr[p.Name]).Returns(MapperCreator(p.PropertyType));
+
+            return mock.Object;
+        }
+
+        [Fact]
+        public void ContainerCreation_TryGetMapper_MapperExist()
+        {
+            var container = new DynamicMapperContainer(new Type[] { typeof(POCO) });
+            Assert.True(container.TryGetMapper<POCO>(out var mapper));
+        }
+
+        [Fact]
+        public void Mapper_MapObject_AllPropertiesMapped()
+        {
+            var container = new DynamicMapperContainer(new Type[] { typeof(POCO) });
+            container.TryGetMapper<POCO>(out var mapper);
+
+            var reader = GetDataReaderMock();
+
+            var list = new List<POCO>();
+            while (reader.Read())
+            {
+                POCO poco = new POCO();
+                mapper(reader, poco);
+                list.Add(poco);
+            }
+
+            foreach (var prop in typeof(POCO).GetProperties())
+                Assert.True(list.TrueForAll(poco => prop.GetValue(poco)?.Equals(dic[prop.PropertyType]) ?? dic[prop.PropertyType] == null),
+                    $"Property {prop.Name} of type {prop.PropertyType.Name} was not mapped correctly.");
+        }
+
+        [Fact]
+        public void ManualMapping_SameOutput()
+        {
+            var reader = GetDataReaderMock();
+            var manualList = new List<POCO>();
+            while (reader.Read())
+            {
+                manualList.Add(new POCO()
+                {
+                    Id = (Guid)reader["Id"],
+                    Name = (string)reader["Name"],
+                    Date = (DateTime)reader["Date"],
+                    Count = (int?)reader["Count"],
+                    Long = (long)reader["Long"],
+                    Question = (bool)reader["Question"]
+                });
+            }
+
+            reader = GetDataReaderMock();
+            var dynamicList = new List<POCO>();
+            var container = new DynamicMapperContainer(new Type[] { typeof(POCO) });
+            container.TryGetMapper<POCO>(out var mapper);
+            while (reader.Read())
+            {
+                var poco = new POCO();
+                mapper(reader, poco);
+                dynamicList.Add(poco);
+            }
+
+            Assert.Equal(manualList.Count, dynamicList.Count);
+            Assert.True(dynamicList.Zip(manualList, (a, b) => a.Equals(b)).All(x => x), "All items in both lists should be the same.");
+            
+        }
+    }
+}
---
+++ DynamicMapper/DynamicMapperContainer.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace DynamicMapper
+{
+    public class DynamicMapperContainer
+    {
+        private readonly Dictionary<Type, object> _dictionnary = new Dictionary<Type, object>();
+
+        public DynamicMapperContainer(Assembly assembly, Func<Type, bool> typeFilter) : this(assembly.GetTypes().Where(typeFilter).ToArray()) { }
+
+        public DynamicMapperContainer(Type[] types)
+        {
+            // [PART 1] Generate the code as string       
+            var containerName = "list";
+            var varPrefix = "type";
+            // declare a new empty dictionary
+            var mapperContainerCode = $"var {containerName} = new Dictionary<Type, object>();";
+            // for each type, create a mapping Action and add it into the dictionary
+            var mapperCreationCode = types.Select(MapperCodeGeneratorFactory(varPrefix));
+            var addMapperToContainerCode = types.Select(AddIntoContainerCodeGeneratorFactory(varPrefix, containerName));
+            // return the dictionary
+            var returnCode = $"return {containerName};";
+
+            // [PART 2] Evaluate the generated code
+            var code = mapperContainerCode + mapperCreationCode.Concat(addMapperToContainerCode).Aggregate(string.Concat) + returnCode;
+            var options = GetScriptOptions(types.Select(t => t.Assembly).Distinct().ToArray());
+            var task = CSharpScript.EvaluateAsync<Dictionary<Type, object>>(code, options);
+            task.Wait();
+
+            // [PART 3] Get the generated code evaluation result       
+            _dictionnary = task.Result;
+        }
+
+        private ScriptOptions GetScriptOptions(params Assembly[] assemblies)
+        {
+            return ScriptOptions.Default.WithImports("System", "System.Collections.Generic", "System.Data").WithReferences(assemblies);
+        }
+
+        private Func<Type, int, string> MapperCodeGeneratorFactory(string varPrefix)
+        {
+            return (type, index) =>
+            {
+                var expression = GetMapperExpression(type);
+                return $"{typeof(Action).Name}<{typeof(IDataReader)}" +
+                    $", {type.FullName}> {varPrefix}{index} = {expression} ;";
+            };
+        }
+
+        private string GetMapperExpression(Type type)
+        {
+            var sourceParam = "source";
+            var targetParam = "target";
+
+            var properties = type.GetProperties().Where(p => p.CanWrite);
+            var code = properties
+                .Select(p => $"{targetParam}.{p.Name} = ({GetPropertyTypeString(p.PropertyType)}){sourceParam}[\"{p.Name}\"];")
+                .Aggregate(string.Concat);
+
+            return $"({sourceParam}, {targetParam}) => {{ {code} }}";
+        }
+
+        private string GetPropertyTypeString(Type propertyType)
+        {
+            if (propertyType.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                return $"Nullable<{propertyType.GetGenericArguments()[0]}>";
+            else
+                return propertyType.ToString();
+        }
+
+        private Func<Type, int, string> AddIntoContainerCodeGeneratorFactory(string varPrefix, string containerName)
+        {
+            return (type, index) => $"{containerName}.Add(typeof({type.FullName}), {varPrefix}{index});";
+        }
+
+        private Action<IDataReader, T> GetSingleMapper<T>()
+        {
+            var type = typeof(T);
+            var task = CSharpScript.EvaluateAsync<Action<IDataReader, T>>(GetMapperExpression(type), GetScriptOptions(type.Assembly));
+            task.Wait();
+            return task.Result;
+        }
+
+        public bool TryGetMapper<T>(out Action<IDataReader, T> mapper)
+        {
+            mapper = null;
+
+            var success = _dictionnary.TryGetValue(typeof(T), out var m);
+            if (success)
+                mapper = (Action<IDataReader, T>)m;
+            else
+            {
+                var type = typeof(T);
+                if (_dictionnary.ContainsKey(type))
+                    return false;
+                else
+                {
+                    _dictionnary.Add(typeof(T), GetSingleMapper<T>());
+                    return TryGetMapper<T>(out mapper);
+                }
+            }
+
+            return success;
+        }
+    }
+}
